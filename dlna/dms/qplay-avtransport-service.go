e2ed45b5e66bc6955877b1cad8468532fd7008e7
Brant Rosenberger <brant.rosenberger.partner@bowerswilkins.com>
2020-01-08 14:17:19 -0500

    Add endpoint stubs for the QPlay services

--- dlna/dms/qplay-avtransport-service.go
+++ dlna/dms/qplay-avtransport-service.go
@@ -12,97 +12,54 @@ type QPlayAVTransportService struct {
 	upnp.Eventing
 }
 
-//TODO: Update the handler to handle the events listed in the QPlay services doc
 func (me *QPlayAVTransportService) Handle(action string, argsXML []byte, r *http.Request) (map[string]string, error) {
-	//host := r.Host
-	//userAgent := r.UserAgent()
 
-    fmt.Println("Handle called! for %s\n", action)
-    return map[string]string{
-        "ThingA": "ThingB",
-    }, nil
+    fmt.Println("QPlayAVTransportService::Handle() called %s\n", action)
 
-    /*
 	switch action {
-	case "GetSystemUpdateID":
+    // Params to this are: "InstanceID", "CurrentURI", "CurrentURIMetaData" 
+	case "SetAVTransportURI":
 		return map[string]string{
-			"Id": me.updateIDString(),
+			"": "",
 		}, nil
-	case "GetSortCapabilities":
+    // Params to this are: "InstanceID", returns (CurrentURI string, CurrentURIMetaData string)
+    case "GetMediaInfo":
 		return map[string]string{
-			"SortCaps": "dc:title",
+			"CurrentURI": "something",
+			"CurrentURIMetadata": "something",
 		}, nil
-	case "Browse":
-		var browse browse
-		if err := xml.Unmarshal([]byte(argsXML), &browse); err != nil {
-			return nil, err
-		}
-		obj, err := me.objectFromID(browse.ObjectID)
-		if err != nil {
-			return nil, upnp.Errorf(upnpav.NoSuchObjectErrorCode, err.Error())
-		}
-		switch browse.BrowseFlag {
-		case "BrowseDirectChildren":
-			objs, err := me.readContainer(obj, host, userAgent)
-			if err != nil {
-				return nil, upnp.Errorf(upnpav.NoSuchObjectErrorCode, err.Error())
-			}
-			totalMatches := len(objs)
-			objs = objs[func() (low int) {
-				low = browse.StartingIndex
-				if low > len(objs) {
-					low = len(objs)
-				}
-				return
-			}():]
-			if browse.RequestedCount != 0 && int(browse.RequestedCount) < len(objs) {
-				objs = objs[:browse.RequestedCount]
-			}
-			result, err := xml.Marshal(objs)
-			if err != nil {
-				return nil, err
-			}
-			return map[string]string{
-				"TotalMatches":   fmt.Sprint(totalMatches),
-				"NumberReturned": fmt.Sprint(len(objs)),
-				"Result":         didl_lite(string(result)),
-				"UpdateID":       me.updateIDString(),
-			}, nil
-		case "BrowseMetadata":
-			fileInfo, err := os.Stat(obj.FilePath())
-			if err != nil {
-				if os.IsNotExist(err) {
-					return nil, &upnp.Error{
-						Code: upnpav.NoSuchObjectErrorCode,
-						Desc: err.Error(),
-					}
-				}
-				return nil, err
-			}
-			upnp, err := me.cdsObjectToUpnpavObject(obj, fileInfo, host, userAgent)
-			if err != nil {
-				return nil, err
-			}
-			buf, err := xml.Marshal(upnp)
-			if err != nil {
-				return nil, err
-			}
-			return map[string]string{
-				"TotalMatches":   "1",
-				"NumberReturned": "1",
-				"Result":         didl_lite(func() string { return string(buf) }()),
-				"UpdateID":       me.updateIDString(),
-			}, nil
-		default:
-			return nil, upnp.Errorf(upnp.ArgumentValueInvalidErrorCode, "unhandled browse flag: %v", browse.BrowseFlag)
-		}
-	case "GetSearchCapabilities":
+    // Params to this are: "InstanceID", returns "PositionInfoResponse"
+    case "GetPositionInfo":
 		return map[string]string{
-			"SearchCaps": "",
+			"PositionInfoResponse": "",
+		}, nil
+    // Params to this are: "InstanceID"
+    case "Stop":
+		return map[string]string{
+			"": "",
+		}, nil
+    // Params to this are: "InstanceID", "Speed"
+    case "Play":
+		return map[string]string{
+			"": "",
+		}, nil
+    // Params to this are: "InstanceID"
+    case "Pause":
+		return map[string]string{
+			"": "",
+		}, nil
+    // Params to this are: "InstanceID", "Speed", "Target"
+    case "Seek":
+		return map[string]string{
+			"": "",
+		}, nil
+    // Params to this are: "InstanceID", "NewPlayMode"
+    case "SetPlayMode":
+		return map[string]string{
+			"": "",
 		}, nil
 	default:
 		return nil, upnp.InvalidActionError
 	}
-    */
 }
 
--- dlna/dms/qplay-renderingcontrol-service.go
+++ dlna/dms/qplay-renderingcontrol-service.go
@@ -12,97 +12,33 @@ type QPlayRenderingControlService struct {
 	upnp.Eventing
 }
 
-//TODO: Update the handler to handle the events listed in the QPlay services doc
 func (me *QPlayRenderingControlService) Handle(action string, argsXML []byte, r *http.Request) (map[string]string, error) {
-	//host := r.Host
-	//userAgent := r.UserAgent()
 
     fmt.Println("QPlayRenderingControlService::Handle()! for %s\n", action)
-    return map[string]string{
-        "ThingA": "ThingB",
-    }, nil
 
-    /*
-	switch action {
-	case "GetSystemUpdateID":
+    switch action {
+    // Params to this are: "InstanceID", "Channel", returns "Mute" bool
+	case "GetMute":
 		return map[string]string{
-			"Id": me.updateIDString(),
+			"Mute": "true",
 		}, nil
-	case "GetSortCapabilities":
+    // Params to this are: "InstanceID", "Channel", "Mute"
+    case "SetMute":
 		return map[string]string{
-			"SortCaps": "dc:title",
+			"": "",
 		}, nil
-	case "Browse":
-		var browse browse
-		if err := xml.Unmarshal([]byte(argsXML), &browse); err != nil {
-			return nil, err
-		}
-		obj, err := me.objectFromID(browse.ObjectID)
-		if err != nil {
-			return nil, upnp.Errorf(upnpav.NoSuchObjectErrorCode, err.Error())
-		}
-		switch browse.BrowseFlag {
-		case "BrowseDirectChildren":
-			objs, err := me.readContainer(obj, host, userAgent)
-			if err != nil {
-				return nil, upnp.Errorf(upnpav.NoSuchObjectErrorCode, err.Error())
-			}
-			totalMatches := len(objs)
-			objs = objs[func() (low int) {
-				low = browse.StartingIndex
-				if low > len(objs) {
-					low = len(objs)
-				}
-				return
-			}():]
-			if browse.RequestedCount != 0 && int(browse.RequestedCount) < len(objs) {
-				objs = objs[:browse.RequestedCount]
-			}
-			result, err := xml.Marshal(objs)
-			if err != nil {
-				return nil, err
-			}
-			return map[string]string{
-				"TotalMatches":   fmt.Sprint(totalMatches),
-				"NumberReturned": fmt.Sprint(len(objs)),
-				"Result":         didl_lite(string(result)),
-				"UpdateID":       me.updateIDString(),
-			}, nil
-		case "BrowseMetadata":
-			fileInfo, err := os.Stat(obj.FilePath())
-			if err != nil {
-				if os.IsNotExist(err) {
-					return nil, &upnp.Error{
-						Code: upnpav.NoSuchObjectErrorCode,
-						Desc: err.Error(),
-					}
-				}
-				return nil, err
-			}
-			upnp, err := me.cdsObjectToUpnpavObject(obj, fileInfo, host, userAgent)
-			if err != nil {
-				return nil, err
-			}
-			buf, err := xml.Marshal(upnp)
-			if err != nil {
-				return nil, err
-			}
-			return map[string]string{
-				"TotalMatches":   "1",
-				"NumberReturned": "1",
-				"Result":         didl_lite(func() string { return string(buf) }()),
-				"UpdateID":       me.updateIDString(),
-			}, nil
-		default:
-			return nil, upnp.Errorf(upnp.ArgumentValueInvalidErrorCode, "unhandled browse flag: %v", browse.BrowseFlag)
-		}
-	case "GetSearchCapabilities":
+    // Params to this are: "InstanceID", "Channel", returns "CurrentVolume"
+    case "GetVolume":
 		return map[string]string{
-			"SearchCaps": "",
+			"CurrentVolume": "50",
+		}, nil
+    // Params to this are: "InstanceID", "Channel", "DesiredVolume"
+    case "SetVolume":
+		return map[string]string{
+			"": "",
 		}, nil
 	default:
 		return nil, upnp.InvalidActionError
 	}
-    */
 }
 
